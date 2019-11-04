f2c007f80166e6da65d893ebd4c19b7676c99cee
Hamidur Rahman <Hamidur0405@gmail.com>
2019-11-03 22:27:47 -0500

    created a new branch with my name, all my changes will be there

--- src/main/java/com/hrs/view/View.java
+++ src/main/java/com/hrs/view/View.java
@@ -3,12 +3,30 @@ package com.hrs.view;
 import com.hrs.controller.Controller;
 import com.hrs.util.Utility;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class View extends Application {
+public class View extends Application
+{
     private Controller controller;
 
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception
+    {
+        Stage stage = new Stage();
+    
+        HBox buttonContainer = new HBox();
+        Button button = new Button("Click");
+        button.setOnAction(e ->
+        {
+            stage.show();
+        });
+        buttonContainer.setAlignment(Pos.BASELINE_CENTER);
+        Scene scene = new Scene(buttonContainer);
+        primaryStage.setScene(scene);
+        buttonContainer.getChildren().add(button);
         primaryStage.setTitle(Utility.APP_TITLE);
         primaryStage.show();
     }
---
+++ src/main/java/com/hrs/view/models/Reservation.java
@@ -0,0 +1,9 @@
+package com.hrs.view.models;
+
+import java.io.Serializable;
+
+public class Reservation implements Serializable
+{
+    private Customer customer;
+    
+}
