97b94ff233277f7b2f96f64d2db82f05710ca80a
SyedHussain <tah94mid@gmail.com>
2019-11-06 13:09:09 -0500

    adding some changes in syed branch

--- src/main/java/com/hrs/service/ApiService.java
+++ src/main/java/com/hrs/service/ApiService.java
@@ -4,6 +4,34 @@ package com.hrs.service;
 /**
  * A service class that provides database access
  */
-public class ApiService {
+public class ApiService implements Service {
+    @Override
+    public void getAllFlights()
+    {
 
+    }
+
+    @Override
+    public void getAllFlightsByAirline(String airlineName)
+    {
+
+    }
+
+    @Override
+    public void validateCustomerLogin(String username, String password)
+    {
+
+    }
+
+    @Override
+    public void validateAirlineAdminLogin(String username, String password)
+    {
+
+    }
+
+    @Override
+    public void insertNewCustomer(String firstName, String lastName, String email, String password)
+    {
+
+    }
 }
---
+++ src/main/java/com/hrs/service/Service.java
@@ -0,0 +1,13 @@
+package com.hrs.service;
+
+/**
+ *  A interface defines all the service methods that will be invoked from View to Backend to retrieve data.
+ */
+public interface Service {
+    public void getAllFlights();
+    public void getAllFlightsByAirline(String airlineName);
+    public void validateCustomerLogin(String username, String password);
+    public void validateAirlineAdminLogin(String username, String password);
+
+    public void insertNewCustomer(String firstName, String lastName, String email, String password);
+}
