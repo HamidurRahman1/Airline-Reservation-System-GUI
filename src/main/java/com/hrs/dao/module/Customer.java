a563efa112b364d6ecab6b763be07f80057f72d4
adnanrahin <adnan.rahin13@gmail.com>
2019-11-06 20:51:00 -0500

    local

--- src/main/java/com/hrs/dao/module/Customer.java
+++ src/main/java/com/hrs/dao/module/Customer.java
@@ -1,13 +1,8 @@
 package com.hrs.dao.module;
 
-public class Customer
-{
+public class Customer {
     String name;
     String address;
-<<<<<<< HEAD
-=======
-    int id;
-    int age;
-    int newlyadded;
->>>>>>> f19a7901555e4d7bf313745111aa6e85a89ced40
+
+    String test_int_local_branch;
 }
